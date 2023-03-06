fbc81724e7f586550060170b3e87ec9facfe2e5f
Ana Rodrigues <afafy1010@gmail.com>
2023-03-06 16:10:07 -0300

    dados 1.7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,15 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import glob
+import os
+import sys
+from os.path import abspath, join, dirname
+from pathlib import Path
+
+sys.path.insert(0, abspath(join(dirname(__file__))))
+sys.path.append(abspath(join(dirname(__file__), "renku-python/docs/_ext")))
+
 project = 'Professional Services'
 copyright = '2023, ProServ'
 author = 'ProServ'
@@ -16,6 +25,10 @@ release = '1.0.0'
 
 extensions = []
 
+plantweb_defaults = {"format": "png"}
+
+master_doc = "index"
+
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -24,4 +37,17 @@ language = 'pt_BR'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+
 html_theme = 'renku'
+
+html_theme_options = {
+    "logo_only": True,
+    "display_version": True,
+    "prev_next_buttons_location": "bottom",
+    "style_external_links": False,
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+    "includehidden": True,
+    "titles_only": False,
+}
