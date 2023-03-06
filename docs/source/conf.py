c627d0e7e38ea45023ce42acced322332c6fe6c1
Ana Rodrigues <afafy1010@gmail.com>
2023-03-06 16:40:44 -0300

    dados 1.10

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['karma_sphinx_theme']
 
 plantweb_defaults = {"format": "png"}
 
@@ -28,5 +28,3 @@ language = 'pt_BR'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-
-html_theme = 'karma_sphinx_theme'
