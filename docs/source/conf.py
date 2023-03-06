044e8b90de3936a79f218db0c845fb446a4d71b7
Ana Rodrigues <afafy1010@gmail.com>
2023-03-06 13:10:23 -0300

    dados 1.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,5 +29,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+import sphinx_pdj_theme
+html_theme = 'sphinx_pdj_theme'
+html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
